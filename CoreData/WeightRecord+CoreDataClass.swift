28b6da8d21719a33f18cafe4696ba2f741f109ad
Siyu <zhousiyut@gmail.com>
2025-04-09 22:59:59 -0400

    Splash screen

---
+++ CoreData/WeightRecord+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WeightRecord+CoreDataClass.swift
+//  MeowMate
+//
+//  Created by Siyu Zhou on 2025-04-06.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WeightRecord)
+public class WeightRecord: NSManagedObject {
+
+}
---
+++ Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  MeowMate
+//
+//  Created by Siyu Zhou on 2025-04-06.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
